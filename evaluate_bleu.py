eb6da08e8e0480adf572c4bdc34bb040801afd14
Célian <celvas@me.com>
2025-06-25 11:46:29 +0200

    Fix : Evaluation BLEU pipeline

--- evaluate_bleu.py
+++ evaluate_bleu.py
@@ -12,20 +12,18 @@ def evaluate_bleu(config, model_path):
     bleu = evaluate.load("sacrebleu")
     predictions, references = [], []
 
-    # Chargement modèle et tokenizer
     tokenizer = Tokenizer.from_file(config['token_path'])
     model = Seq2SeqModel(
         d_model=config["d_model"], h=config["h"], d_ff=config["d_ff"],
         vocab_size=tokenizer.get_vocab_size()
     )
 
-    device = "cpu" # torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.load_state_dict(torch.load(model_path, map_location=device))
     model.to(device)
     print(device)
     model.eval()
 
-    # Chargement data
     dataloaders = get_dataloaders(
         config["lang1"], config["lang2"], config["token_path"],
         config["data_path"], batch_size=config["batch_size"],
@@ -36,22 +34,32 @@ def evaluate_bleu(config, model_path):
         for src_batch, tgt_batch in tqdm(dataloaders['test']):
             src_batch = src_batch.to(device)
 
-            # Génération auto-regressive (greedy)
             generated = torch.full_like(src_batch[:, :1], tokenizer.token_to_id("[BOS]"))
+            is_finished = torch.zeros(generated.size(0), dtype=torch.bool, device=device)
+            # For each sequence of the batch we have a boolean that indicate if the generation is over or not
+            # the model generated EOS token
+
             for _ in range(config["max_length"]):
                 out = model(src_batch, generated)
-                next_token = out[:, -1, :].argmax(dim=-1, keepdim=True)
-                generated = torch.cat([generated, next_token], dim=1)
+                next_token = out[:, -1, :].argmax(dim=-1)
 
-                # Early stop si tous les tokens sont <EOS>
-                if (next_token == tokenizer.token_to_id("[EOS]")).all():
+                next_token = torch.where(
+                    is_finished, 
+                    torch.full_like(next_token, tokenizer.token_to_id("[PAD]")), 
+                    next_token
+                )
+                # If the sentence is fully generated we fill the rest of it with PAD token
+
+                generated = torch.cat([generated, next_token.unsqueeze(1)], dim=1)
+
+                is_finished |= next_token == tokenizer.token_to_id("[EOS]")
+                # Update the finish status of each sequence
+
+                if is_finished.all():
                     break
 
             pred_sentences = [tokenizer.decode(ids, skip_special_tokens=True) for ids in generated.tolist()]
             ref_sentences =  [tokenizer.decode(ids, skip_special_tokens=True) for ids in tgt_batch.tolist()]
-
-            print(f"Predicted: {pred_sentences}")
-            print(f"Reference: {ref_sentences}")
 
             predictions.extend(pred_sentences)
             references.extend([[ref] for ref in ref_sentences])  # BLEU = list of list of references
@@ -69,4 +77,4 @@ if __name__ == "__main__":
 
     model_path = f"save/{args.save_name}/best_model.pt"
     bleu_score = evaluate_bleu(config, model_path)
-    print(f"BLEU score: {bleu_score['bleu']:.2f}")
+    print(f"BLEU score: {bleu_score['score']:.2f}")
